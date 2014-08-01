d1b7364f3ff4e5e3c52779b734ecc13f516d87c5
sadmin <jchudzynski@uwf.edu>
2014-08-01 08:46:37 -0500

    schedule

---
+++ Brackets/ViewControllers/JMCScheduleViewController.h
@@ -0,0 +1,13 @@
+//
+//  JMCScheduleViewController.h
+//  Brackets
+//
+//  Created by sadmin on 7/31/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JMCScheduleViewController : UIViewController
+
+@end
---
+++ Brackets/ViewControllers/JMCScheduleViewController.m
@@ -0,0 +1,49 @@
+//
+//  JMCScheduleViewController.m
+//  Brackets
+//
+//  Created by sadmin on 7/31/14.
+//  Copyright (c) 2014 Janusz Chudzynski. All rights reserved.
+//
+
+#import "JMCScheduleViewController.h"
+
+@interface JMCScheduleViewController ()
+
+@end
+
+@implementation JMCScheduleViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
